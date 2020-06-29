7f2692f84c7aa1c66208044a8b906379a0816a88
LYNN DAMOTA <bdamota0506@gmail.com>
2020-06-28 22:57:48 -0700

    logic added for incorrect user input

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,39 +9,25 @@
 
 
 fetch(
-  "https://api.openweathermap.org/data/2.5/forecast?q=atlanta&units=imperial&appid=1506e7234cdde16a04f0552631be2b2d"
+  "https://api.openweathermap.org/data/2.5/forecast?q=istanbul&units=imperial&appid=1506e7234cdde16a04f0552631be2b2d"
 )
   // Converts the response to JSON
   .then(function(response) {
+    if (response.ok){
     return response.json();
+    } 
+    else {
+      alert("Enter correct city name.")
+    }
   })
   .then(function(json) {
-    //Use 'querySelector' to get the ID of where elements will be displayed
-    // var name = document.getElementById("name");
-    // var date = moment().format('M/DD/YYYY');
-    // var image = document.getElementById("image")
-    // var temp = document.getElementById("temp");
-    // var humidity = document.getElementById("humidity");
-    // var wind = document.getElementById("wind");
-    // var UV = document.getElementById("UV");
-    // var iconUrl = "http://openweathermap.org/img/w/" + json.list[0].weather[0].icon + ".png"; 
-
-
-    // // Main Weather Container Content 
-    //  name.innerHTML = json.city.name + " (" + date + ")";
-    //  image.innerHTML= " <img src=" + iconUrl +">"; 
-    //  temp.innerHTML = "Temperature: " + json.list[0].main.temp + " °F";
-    //  humidity.innerHTML = "Humidity: " + json.list[0].main.humidity + "%";
-    //  wind.innerHTML = "Wind Speed: " + json.list[0].wind.speed + " MPH"; 
-    //  UV.innerHTML = "UV Index: "
-    
      var date = moment().format('M/DD/YYYY');
      var iconUrl = "http://openweathermap.org/img/w/" + json.list[0].weather[0].icon + ".png"; 
      
      $("#weatherContainer").append(
        "<div class='listGroup col-12 col-md-12'>"
        +  "<h3 class='oneDayTitle'>" +  json.city.name + " (" + date + ")" +"</h3>"
-       +  "<div class='oneDay'>"  + "<img src=" + iconUrl +">" + "</div>"
+       +  "<div class='oneDayIcon'>"  + "<img src=" + iconUrl +">" + "</div>"
        +  "<div class='oneDay'>" + "Temperature: " + json.list[0].main.temp + " °F" + "</div>"
        +  "<div class='oneDay'>" + "Humidity: " + json.list[0].main.humidity + "%" + "</div>" 
        +  "<div class='oneDay'>" + "Wind Speed: " + json.list[0].wind.speed + " MPH" + "</div>" 
