7947d1a30e183a756884efe46823a4ec0d576b97
LYNN DAMOTA <bdamota0506@gmail.com>
2020-06-28 22:35:05 -0700

    append weather container

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,23 +17,38 @@ fetch(
   })
   .then(function(json) {
     //Use 'querySelector' to get the ID of where elements will be displayed
-    var name = document.getElementById("name");
-    var date = moment().format('M/DD/YYYY');
-    var temp = document.getElementById("temp");
-    var humidity = document.getElementById("humidity");
-    var wind = document.getElementById("wind");
-    var UV = document.getElementById("UV");
-    var iconUrl = "http://openweathermap.org/img/w/" + json.list[0].weather[0].icon + ".png"; 
+    // var name = document.getElementById("name");
+    // var date = moment().format('M/DD/YYYY');
+    // var image = document.getElementById("image")
+    // var temp = document.getElementById("temp");
+    // var humidity = document.getElementById("humidity");
+    // var wind = document.getElementById("wind");
+    // var UV = document.getElementById("UV");
+    // var iconUrl = "http://openweathermap.org/img/w/" + json.list[0].weather[0].icon + ".png"; 
 
 
-    // Main Weather Container Content 
-     name.innerHTML = json.city.name + " (" + date + ")" + " <img src=" + iconUrl +">"; 
-     temp.innerHTML = "Temperature: " + json.list[0].main.temp + " °F";
-     humidity.innerHTML = "Humidity: " + json.list[0].main.humidity + "%";
-     wind.innerHTML = "Wind Speed: " + json.list[0].wind.speed + " MPH"; 
-     UV.innerHTML = "UV Index: "  
+    // // Main Weather Container Content 
+    //  name.innerHTML = json.city.name + " (" + date + ")";
+    //  image.innerHTML= " <img src=" + iconUrl +">"; 
+    //  temp.innerHTML = "Temperature: " + json.list[0].main.temp + " °F";
+    //  humidity.innerHTML = "Humidity: " + json.list[0].main.humidity + "%";
+    //  wind.innerHTML = "Wind Speed: " + json.list[0].wind.speed + " MPH"; 
+    //  UV.innerHTML = "UV Index: "
+    
+     var date = moment().format('M/DD/YYYY');
+     var iconUrl = "http://openweathermap.org/img/w/" + json.list[0].weather[0].icon + ".png"; 
      
-     
+     $("#weatherContainer").append(
+       "<div class='listGroup col-12 col-md-12'>"
+       +  "<h3 class='oneDayTitle'>" +  json.city.name + " (" + date + ")" +"</h3>"
+       +  "<div class='oneDay'>"  + "<img src=" + iconUrl +">" + "</div>"
+       +  "<div class='oneDay'>" + "Temperature: " + json.list[0].main.temp + " °F" + "</div>"
+       +  "<div class='oneDay'>" + "Humidity: " + json.list[0].main.humidity + "%" + "</div>" 
+       +  "<div class='oneDay'>" + "Wind Speed: " + json.list[0].wind.speed + " MPH" + "</div>" 
+       +  "<div class='oneDay'>" + "UV Index: "  + "</div>" 
+       +
+       "</div>"
+     )
      
     // Five Day Header
     $("#fiveDay").append(
