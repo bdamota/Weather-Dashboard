17ae1fc72b6ea0d9a0c7b0070ab037e5e1a2542d
LYNN DAMOTA <bdamota0506@gmail.com>
2020-06-28 23:33:42 -0700

    list updated for weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,15 +42,15 @@ fetch(
   })
   .then(function(json) {
      var date = moment().format('M/DD/YYYY');
-     var iconUrl = "http://openweathermap.org/img/w/" + json.list[0].weather[0].icon + ".png"; 
+     var iconUrl = "http://openweathermap.org/img/w/" + json.list[2].weather[0].icon + ".png"; 
      
      $("#weatherContainer").append(
        "<div class='listGroup col-12 col-md-12'>"
        +  "<h3 class='oneDayTitle'>" +  json.city.name + " (" + date + ")" +"</h3>"
        +  "<div class='oneDayIcon'>"  + "<img src=" + iconUrl +">" + "</div>"
-       +  "<div class='oneDay'>" + "Temperature: " + json.list[0].main.temp + " °F" + "</div>"
-       +  "<div class='oneDay'>" + "Humidity: " + json.list[0].main.humidity + "%" + "</div>" 
-       +  "<div class='oneDay'>" + "Wind Speed: " + json.list[0].wind.speed + " MPH" + "</div>" 
+       +  "<div class='oneDay'>" + "Temperature: " + json.list[2].main.temp + " °F" + "</div>"
+       +  "<div class='oneDay'>" + "Humidity: " + json.list[2].main.humidity + "%" + "</div>" 
+       +  "<div class='oneDay'>" + "Wind Speed: " + json.list[2].wind.speed + " MPH" + "</div>" 
        +  "<div class='oneDay'>" + "UV Index: "  + "</div>" 
        +
        "</div>"
@@ -65,62 +65,62 @@ fetch(
     ); 
 
     // Day 1 Forecast Content
-    var iconUrl = "http://openweathermap.org/img/w/" + json.list[1].weather[0].icon + ".png";
+    var iconUrl = "http://openweathermap.org/img/w/" + json.list[10].weather[0].icon + ".png";
 
     $("#day1").append(
       "<div class='forecast card col s12 m6'>"
       +  "<div class='card-title'>" +  moment().add(1, 'days').format('M/DD/YYYY') +"</div>"
       +  "<div class='card-text'>"  + "<img src=" + iconUrl +">" + "</div>"
-      +  "<div class='card-text'>" + "Temp: " + json.list[1].main.temp + " °F" + "</div>"
-      +  "<div class='card-text'>" + "Humidity: " + json.list[1].main.humidity + "%" + "</div>" 
+      +  "<div class='card-text'>" + "Temp: " + json.list[10].main.temp + " °F" + "</div>"
+      +  "<div class='card-text'>" + "Humidity: " + json.list[10].main.humidity + "%" + "</div>" 
       + "</div>" 
      ); 
 
      // Day 2 Forecast Content
-     var iconUrl = "http://openweathermap.org/img/w/" + json.list[2].weather[0].icon + ".png";
+     var iconUrl = "http://openweathermap.org/img/w/" + json.list[18].weather[0].icon + ".png";
 
      $("#day2").append(
       "<div class='forecast card col s12 m6'>"
       +  "<div class='card-title'>" +  moment().add(2, 'days').format('M/DD/YYYY') +"</div>"
       +  "<div class='card-text'>"  + "<img src=" + iconUrl +">" + "</div>"
-      +  "<div class='card-text'>" + "Temp: " + json.list[2].main.temp + " °F" + "</div>"
-      +  "<div class='card-text'>" + "Humidity: " + json.list[2].main.humidity + "%" + "</div>" 
+      +  "<div class='card-text'>" + "Temp: " + json.list[18].main.temp + " °F" + "</div>"
+      +  "<div class='card-text'>" + "Humidity: " + json.list[18].main.humidity + "%" + "</div>" 
       + "</div>" 
      ); 
 
     // Day 3 Forecast Content 
-    var iconUrl = "http://openweathermap.org/img/w/" + json.list[3].weather[0].icon + ".png";
+    var iconUrl = "http://openweathermap.org/img/w/" + json.list[26].weather[0].icon + ".png";
 
     $("#day3").append(
       "<div class='forecast card col s12 m6'>"
       +  "<div class='card-title'>" +  moment().add(3, 'days').format('M/DD/YYYY') +"</div>"
       +  "<div class='card-text'>"  + "<img src=" + iconUrl +">" + "</div>"
-      +  "<div class='card-text'>" + "Temp: " + json.list[3].main.temp + " °F" + "</div>"
-      +  "<div class='card-text'>" + "Humidity: " + json.list[3].main.humidity + "%" + "</div>" 
+      +  "<div class='card-text'>" + "Temp: " + json.list[26].main.temp + " °F" + "</div>"
+      +  "<div class='card-text'>" + "Humidity: " + json.list[26].main.humidity + "%" + "</div>" 
       + "</div>" 
      ); 
 
      // Day 4 Forecast Content 
-     var iconUrl = "http://openweathermap.org/img/w/" + json.list[4].weather[0].icon + ".png";
+     var iconUrl = "http://openweathermap.org/img/w/" + json.list[34].weather[0].icon + ".png";
 
      $("#day4").append(
       "<div class='forecast card col s12 m6'>"
       +  "<div class='card-title'>" +  moment().add(4, 'days').format('M/DD/YYYY') +"</div>"
       +  "<div class='card-text'>"  + "<img src=" + iconUrl +">" + "</div>"
-      +  "<div class='card-text'>" + "Temp: " + json.list[4].main.temp + " °F" + "</div>"
-      +  "<div class='card-text'>" + "Humidity: " + json.list[4].main.humidity + "%" + "</div>" 
+      +  "<div class='card-text'>" + "Temp: " + json.list[34].main.temp + " °F" + "</div>"
+      +  "<div class='card-text'>" + "Humidity: " + json.list[34].main.humidity + "%" + "</div>" 
       + "</div>" 
      ); 
 
     // Day 5 Forecast Content 
-    var iconUrl = "http://openweathermap.org/img/w/" + json.list[5].weather[0].icon + ".png";
+    var iconUrl = "http://openweathermap.org/img/w/" + json.list[39].weather[0].icon + ".png";
 
     $("#day5").append(
       "<div class='forecast card col s12 m6'>"
       +  "<div class='card-title'>" +  moment().add(5, 'days').format('M/DD/YYYY') +"</div>"
       +  "<div class='card-text'>"  + "<img src=" + iconUrl +">" + "</div>"
-      +  "<div class='card-text'>" + "Temp: " + json.list[5].main.temp + " °F" + "</div>"
-      +  "<div class='card-text'>" + "Humidity: " + json.list[5].main.humidity + "%" + "</div>" 
+      +  "<div class='card-text'>" + "Temp: " + json.list[39].main.temp + " °F" + "</div>"
+      +  "<div class='card-text'>" + "Humidity: " + json.list[39].main.humidity + "%" + "</div>" 
       + "</div>" 
      ); 
   });
